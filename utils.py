b3e3275698e81e75ca10593df235c0427cac8b92
IagoGarciaSuarez <iago.garcia.suarez.ds@gmail.com>
2021-11-25 21:04:51 +0100

    Program finished. Need improvements.
    Need to add function to add time to a worktime data file.

---
+++ utils.py
@@ -0,0 +1,17 @@
+HELP_INFO = '''
+Available commands:
+> create <project>  --> Creates a project registry.
+> start <project>   --> Starts the timer for the current session in a given project.
+> stop              --> Stops the timer for the current session and project.
+> add [-minutes | -hours] <time> <project> --> Adds time in hours or minutes to a given project.
+'''
+
+WORK_TIME_BANNER = '''
+ ____ ____ ____ ____ _________ ____ ____ ____ ____ 
+||W |||o |||r |||k |||       |||T |||i |||m |||e ||
+||__|||__|||__|||__|||_______|||__|||__|||__|||__||
+|/__\|/__\|/__\|/__\|/_______\|/__\|/__\|/__\|/__\|
+
+'''
+
+TIME_FORMAT = '%d-%m-%Y - %H:%M:%S'
